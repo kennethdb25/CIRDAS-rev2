e78a26eea678f97703056d462e674028bf627566
Bautista, Kenneth <kenneth.bautista@accenture.com>
2022-10-26 10:01:44 +0800

    updating config

---
+++ server/config/dev.js
@@ -0,0 +1,7 @@
+//dev.js - don't commit this
+module.exports = {
+	mongoURI: "mongodb+srv://CIRDAS:password10@cirdas.gecdquq.mongodb.net/Authusers?retryWrites=true&w=majority",
+	cookieKeyCitizen: "CIRDASCIRDASCIRDASajkdjhsadkhlakjklahdjlahdahd",
+	cookieKeyPolice: "CIRDASCIRDASCIRDASpoliceaskdlajkajd",
+	cookieKeyAdmin: "CIRDASCIRDASCIRDASadminklsadjlkajdlkjad",
+};
